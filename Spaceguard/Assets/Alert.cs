6c4f06b9366b2df136b025ba45962f3ebe59c6d5
gamerblox <gamerblox10@gmail.com>
2019-04-09 00:53:29 -0700

    Alerts update
    Small update including basic alerts

---
+++ Spaceguard/Assets/Alert.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alert : MonoBehaviour
+{
+    
+    public void RemoveAlert()
+    {
+        Destroy(this.gameObject);
+
+    }
+
+}
---
+++ Spaceguard/Assets/Scripts/AlertsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlertsManager : MonoBehaviour
+{
+    public GameObject menuAlert;
+    public GameObject listAlert;
+
+    void Start()
+    {
+        AddAlert("New asteroid detected!");
+        AddAlert("Test alert.");
+
+    }
+
+    void AddAlert(string data)
+    {
+        GameObject tempButton = Instantiate(menuAlert);
+        tempButton.transform.SetParent(listAlert.transform);
+        tempButton.GetComponentInChildren<Text>().text = data;
+
+    }
+
+    void DeleteAlert()
+    {
+
+
+    }
+
+}
